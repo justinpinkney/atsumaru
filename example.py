650879673c3842cf5748d8da2136e443b7284273
Justin <justinpinkney@gmail.com>
2018-10-09 21:37:50 +0100

    clean up after curses

--- example.py
+++ example.py
@@ -10,19 +10,21 @@ if __name__ == "__main__":
     curses.noecho()
     curses.cbreak()
 
-    canvas_size = (50, 50)
-    canvas = atsumaru.Canvas(canvas_size)
-    patch = atsumaru.Patch([])
-    canvas.insert(patch, (25, 25))
-    for step in range(canvas_size[0]*canvas_size[1]):
-        available = canvas.available
-        chosen_position = random.sample(available, 1)
-        canvas.insert(patch, chosen_position[0])
-        to_print = canvas.__repr__()
-        stdscr.addstr(0, 0, to_print)
-        stdscr.refresh()
-        time.sleep(1/60)
+    try:
+        canvas_size = (50, 50)
+        canvas = atsumaru.Canvas(canvas_size)
+        patch = atsumaru.Patch([])
+        canvas.insert(patch, (25, 25))
+        for step in range(canvas_size[0]*canvas_size[1]):
+            available = canvas.available
+            chosen_position = random.sample(available, 1)
+            canvas.insert(patch, chosen_position[0])
+            to_print = canvas.__repr__()
+            stdscr.addstr(0, 0, to_print)
+            stdscr.refresh()
+            time.sleep(1/60)
 
-    curses.echo()
-    curses.nocbreak()
-    curses.endwin()
+    finally:
+        curses.echo()
+        curses.nocbreak()
+        curses.endwin()
